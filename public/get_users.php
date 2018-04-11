3b240d14d602b8331e34eb39709da122e83a303f
AkashG6 <akashgarule@gmail.com>
2018-04-12 01:27:02 +0530

    add sign in validation and backend. login incomplete

---
+++ public/get_users.php
@@ -0,0 +1,32 @@
+<?php
+
+$DB_HOST = 'localhost';
+$DB_NAME = 'se';
+$DB_USER = 'root';
+$DB_PASS = '';
+
+// Create connection
+$conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql1 = "SELECT username FROM users";
+$result = $conn->query($sql1);
+$num_rows = mysqli_num_rows($result);
+while($row = mysqli_fetch_assoc($result))
+    $test[] = $row;
+if ($num_rows > 0) {
+	echo json_encode($test);
+}
+else
+{
+	echo "None";
+}
+
+
+$conn->close();
+
+?>
---
+++ public/signup.php
@@ -0,0 +1,31 @@
+<?php
+
+$DB_HOST = 'localhost';
+$DB_NAME = 'se';
+$DB_USER = 'root';
+$DB_PASS = '';
+
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$eid = $_POST['eid'];
+$uname = $_POST['uname'];
+$pass = $_POST['pass'];
+
+// Create connection
+$conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql1 = "INSERT INTO users (first_name,last_name,email_id,username,password) VALUES ('$fname','$lname','$eid','$uname','$pass')";
+$result = $conn->query($sql1);
+if ($result) {
+	header('Location: chapters.html'); 
+} 
+
+
+$conn->close();
+
+?>
